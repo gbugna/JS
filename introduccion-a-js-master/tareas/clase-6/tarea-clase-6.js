022cd0cee59c5715dbe34858981871ca7e7d2fcc
gbugna <42363665+gbugna@users.noreply.github.com>
2020-02-02 13:42:10 -0300

    a

--- introduccion-a-js-master/tareas/clase-6/tarea-clase-6.js
+++ introduccion-a-js-master/tareas/clase-6/tarea-clase-6.js
@@ -11,29 +11,61 @@ document.querySelector('#btnProceso').onclick = function empezarProceso() {
 
     const cantidadGrupoFamiliar = Number(prompt("cuantos son en tu familia?"));
 
-    const body = document.querySelector('body');
+    const body = document.querySelector('#anexar');
     const divContenedor = document.createElement('div');
     body.appendChild(divContenedor);
 
 
-    for (let i = 0; i < cantidadGrupoFamiliar; i++) {
+    for (let i = 1; i < cantidadGrupoFamiliar+1 ; i++) {
 
         let input = document.createElement('input');
         let label = document.createElement('label');
-        input.setAttribute("type", "text");
+        let br = document.createElement('br');
+        input.setAttribute("type", "number");
         input.setAttribute("id", i);
-        divContenedor.appendChild(input);
         divContenedor.appendChild(label);
+        labelText = document.createTextNode('ingrese edad de familiar N° '+ i);
+        label.appendChild(labelText);
+        divContenedor.appendChild(input);
+        divContenedor.appendChild(br);
     }
-
-    const btnCalcular = document.createElement('button');
-    const btnLabel = document.createTextNode("calcular");
-    divContenedor.appendChild(btnCalcular);
-    btnCalcular.appendChild(btnLabel);
-
 
 }
 
+    
+
+function realizarCalculo() {
+
+    const edades = document.querySelectorAll('input');
+    let edadesArray = [];
+    
+    for (let i = 0; i < edades.length; i++) {
+
+        edadesArray.push(Number(edades[i].value));
+
+    }
+
+    let totalEdades = 0;
+    for (let i=0; i < edadesArray.length;i++) {
+
+        totalEdades += edadesArray[i];
+           
+    }
+
+
+    function edadMaxima() {
+     let edadMaxima = Math.max.apply(Math, edadesArray);
+     return edadMaxima; 
+    }
+    
+    function edadMinima() {
+        
+    return edadMinima =  Math.min.apply(Math, edadesArray);
+
+}
+    const divEdadMaxima = document.querySelector('#edadMaxima');
+    divEdadMaxima.innerHTML = edadMaxima;
+}
 
 
 
