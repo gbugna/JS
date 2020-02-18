dc785453248c1eb46292ec847b7ada7417cb046d
Gustavo <gbugna@gmail.com>
2020-02-18 15:56:35 -0300

    agregado datepicker

--- js-intermedio-master/clase-12/clase12.js
+++ js-intermedio-master/clase-12/clase12.js
@@ -1,31 +1,34 @@
-
 //fetch API: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
 //usa promesas
-const encabezado = document.querySelector('h2');
-const lista = document.querySelector('#lista');
+const encabezado = document.querySelector("h2");
+const lista = document.querySelector("#lista");
+const datePicker = document.querySelector("#datePicker");
 
+let consultaDefault = "https://api.exchangeratesapi.io/latest";
 
+consultarDivisas(consultaDefault);
 
+datePicker.addEventListener("change", event => {
+  let fecha = event.target.value;
+  consultarDivisas("https://api.exchangeratesapi.io/" + fecha);
+});
 
+function consultarDivisas(consulta) {
+  fetch(consulta)
+    .then(respuesta => respuesta.json())
+    .then(respuestaJSON => {
+      encabezado.textContent = respuestaJSON.base;
 
-fetch("https://api.exchangeratesapi.io/latest")
-  .then(respuesta => respuesta.json())
-  .then(respuestaJSON => {
+      Object.entries(respuestaJSON.rates).forEach(([moneda, valor]) => {
+        let itemLista = document.createElement("li");
+        itemLista.classList.add("list-group-item");
+        itemLista.classList.add("list-group-flush");
+        lista.appendChild(itemLista);
 
-    encabezado.textContent= respuestaJSON.base;
+        let values = document.createTextNode(moneda + valor);
 
-    Object.entries(respuestaJSON.rates).forEach(([moneda, valor]) => {
-    
-    let itemLista = document.createElement("li");
-    itemLista.classList.add("list-group-item");
-    lista.appendChild(itemLista);
-
-    let values = document.createTextNode(moneda+valor);
-       
-    itemLista.appendChild(values);
-      
-
-    });
-  })
-  .catch(error => console.error("FALLÓ", error));
-
+        itemLista.appendChild(values);
+      });
+    })
+    .catch(error => console.error("FALLÓ", error));
+}
