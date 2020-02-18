9b4db7cb4280179a45eee86b4457fcb3e6a237f1
gbugna <42363665+gbugna@users.noreply.github.com>
2020-02-17 22:38:46 -0300

    agregado date picker y creacion de ul

--- js-intermedio-master/clase-12/clase12.js
+++ js-intermedio-master/clase-12/clase12.js
@@ -1,15 +1,28 @@
 
 //fetch API: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
 //usa promesas
+const encabezado = document.querySelector('h2');
+const lista = document.querySelector('#lista');
+
+
+
+
+
 fetch("https://api.exchangeratesapi.io/latest")
   .then(respuesta => respuesta.json())
   .then(respuestaJSON => {
 
-    console.log(respuestaJSON.base);
-    
-    Object.keys(respuestaJSON.rates).forEach(moneda => {
+    encabezado.textContent= respuestaJSON.base;
 
-      document.createElement(`<li>${moneda}: ${respuestaJSON.rates[moneda]}</li>`);
+    Object.entries(respuestaJSON.rates).forEach(([moneda, valor]) => {
+    
+    let itemLista = document.createElement("li");
+    itemLista.classList.add("list-group-item");
+    lista.appendChild(itemLista);
+
+    let values = document.createTextNode(moneda+valor);
+       
+    itemLista.appendChild(values);
       
 
     });
