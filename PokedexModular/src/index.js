02c667e814821d171842546c619e2ebae5d66fab
gbugna <42363665+gbugna@users.noreply.github.com>
2020-03-04 21:49:46 -0300

    Update index.js

--- PokedexModular/src/index.js
+++ PokedexModular/src/index.js
@@ -3,6 +3,6 @@ import { traerPokemonIndividual, traerPokemones } from './consultas.js';
 const divMuestraPokemon = document.querySelector('#DivMuestraPokemon');
 
 
-traerPokemones();
+consulta = traerPokemones();
 divMuestraPokemon.textContent = consulta;
 
