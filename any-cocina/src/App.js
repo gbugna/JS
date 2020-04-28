ecf840ad2415cc9be839434ad7ebb1e4e3d78009
gbugna <42363665+gbugna@users.noreply.github.com>
2020-04-27 22:03:39 -0300

    Added AnyCocina Project

---
+++ any-cocina/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App"></header>
+      <h4>Any Cocina</h4>
+      <input type="text" />
+      <input type="password" name="" id="" />
+    </div>
+  );
+}
+
+export default App;
