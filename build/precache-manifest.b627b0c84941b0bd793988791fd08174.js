25f4c8faef8e05df38799558facef6f085223b8e
gbugna <42363665+gbugna@users.noreply.github.com>
2020-07-22 21:05:35 -0300

    add build

---
+++ build/precache-manifest.b627b0c84941b0bd793988791fd08174.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "325941e8bd79f69868e2681e57635402",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f335a0147424d1587d04",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "127dff3dccdb1bc7a1d3",
+    "url": "/static/js/2.de9c571f.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/static/js/2.de9c571f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f335a0147424d1587d04",
+    "url": "/static/js/main.25d4734d.chunk.js"
+  },
+  {
+    "revision": "9455efcbbf461cd77e18",
+    "url": "/static/js/runtime-main.3a7b2d97.js"
+  }
+]);
